8b6cc1fcf5662e0b8da1695789abd6599d437bd5
RajarshiCode <rajarshidas47@gmail.com>
2024-08-05 22:51:25 +0530

    Configures Database
    added two tables ,

---
+++ spring-stream-backend/src/main/java/com/stream/app/spring_stream_backend/entities/Course.java
@@ -0,0 +1,21 @@
+package com.stream.app.spring_stream_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "streamVideoCourses")
+
+public class Course {
+    @Id
+    private String id;
+
+    private String title;
+//    @OneToMany(mappedBy = "course")
+//    private List<Video> list = new ArrayList<>();
+}
