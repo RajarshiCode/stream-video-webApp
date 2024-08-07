835b22238beb3ead0ae2ac473cd82d7265175151
RajarshiCode <rajarshidas47@gmail.com>
2024-08-07 20:46:03 +0530

    added video path

---
+++ spring-stream-backend/src/main/java/com/stream/app/spring_stream_backend/controllers/VideoController.java
@@ -0,0 +1,14 @@
+package com.stream.app.spring_stream_backend.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/videos")
+public class VideoController {
+
+
+//    public ResponseEntity<CustomMessage>;
+}
--- spring-stream-backend/src/main/java/com/stream/app/spring_stream_backend/services/impl/VideoServiceImpl.java
+++ spring-stream-backend/src/main/java/com/stream/app/spring_stream_backend/services/impl/VideoServiceImpl.java
@@ -2,14 +2,45 @@ package com.stream.app.spring_stream_backend.services.impl;
 
 import com.stream.app.spring_stream_backend.entities.Video;
 import com.stream.app.spring_stream_backend.services.VideoService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class VideoServiceImpl implements VideoService {
 
+    @Value("${files.video}")
+    String DIR;
+
+
+
+
     @Override
     public Video save(Video video, MultipartFile file) {
+
+
+        try{
+            //original filename
+            String filename = file.getOriginalFilename();
+
+            String contentType = file.getContentType();
+            InputStream inputStream  = file.getInputStream();
+
+            String cleanFileName = StringUtils.cleanPath(filename);
+            String cleanFolder = StringUtils.cleanPath(DIR);
+            Path path = Paths.get( cleanFolder, cleanFileName);
+            System.out.println(path);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+
         return null;
     }
 
